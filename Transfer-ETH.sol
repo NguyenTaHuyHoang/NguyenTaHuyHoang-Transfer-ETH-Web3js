98c18502db513d6545e3a3e0f4845bab583b2240
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-06-07 19:52:53 +0700

    Update Transfer-ETH.sol

--- Transfer-ETH.sol
+++ Transfer-ETH.sol
@@ -4,7 +4,6 @@ contract EthTransfer {
     // Hàm để gửi ETH
     function sendEth(address payable recipient) public payable {
         require(msg.value > 0, "Must send some ETH");
-        require(address(this).balance > msg.value );
         recipient.transfer(msg.value);
     }
 }
